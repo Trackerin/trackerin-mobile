902f08715cce1697c0a7de21439d07d1b606e2f1
Vinneth <anandakevinputra208@gmail.com>
2026-04-17 21:49:49 +0700

    add: color setup

--- app/src/main/java/com/example/trackerinmobile/ui/theme/Color.kt
+++ app/src/main/java/com/example/trackerinmobile/ui/theme/Color.kt
@@ -2,6 +2,22 @@ package com.example.trackerinmobile.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// ==========================================
+// Figma Variables
+// ==========================================
+
+val PrimaryBlue = Color(0xFF2F6CF9) // 1. biru utama
+val PrimaryBlueHover = Color(0xFF275ACF) // 7. biru utama berubah jika di animasi hover
+val Black = Color(0xFF091632) // 2. hitam
+val BlackishBlue = Color(0xFF102453) // 8. hitam agak biru
+val BackgroundApp = Color(0xFFECF0F1) // 3. putih modif (sebagai warna background app)
+val WhitePure = Color(0xFFFFFFFF) // 4. putih mutlak
+val TextGray = Color(0xFF6B7280) // 5. Abu abu untuk teks
+val ComponentGray = Color(0xFF9CA3AF) // 6. Abu abu untuk button dan card
+
+// ==========================================
+// Base Colors (Biarkan default M3 untuk referensi)
+// ==========================================
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)

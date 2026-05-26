c460aeeb12078a3d5b3eca108c722ed50d7b68a2
Vinneth <anandakevinputra208@gmail.com>
2026-05-26 17:06:41 +0700

    add: data layer on edit profile

--- app/src/main/java/com/example/trackerinmobile/data/local/TokenManager.kt
+++ app/src/main/java/com/example/trackerinmobile/data/local/TokenManager.kt
@@ -34,13 +34,32 @@ class TokenManager(context: Context) {
         return sharedPreferences.getString(KEY_USER_NAME, null)
     }
 
+    fun saveOccupation(occupation: String) {
+        sharedPreferences.edit().putString(KEY_OCCUPATION, occupation).apply()
+    }
+
+    fun getOccupation(): String? {
+        return sharedPreferences.getString(KEY_OCCUPATION, "College Student, 4th Semester")
+    }
+
+    fun saveSpecialization(spec: String) {
+        sharedPreferences.edit().putString(KEY_SPECIALIZATION, spec).apply()
+    }
+
+    fun getSpecialization(): String? {
+        return sharedPreferences.getString(KEY_SPECIALIZATION, "Fullstack Developer")
+    }
+
     fun clearToken() {
-        sharedPreferences.edit().remove(KEY_TOKEN).remove(KEY_USER_NAME).apply()
+        sharedPreferences.edit().remove(KEY_TOKEN).remove(KEY_USER_NAME)
+            .remove(KEY_OCCUPATION).remove(KEY_SPECIALIZATION).apply()
     }
 
     companion object {
         private const val KEY_TOKEN = "jwt_token"
         private const val KEY_USER_NAME = "user_name"
+        private const val KEY_OCCUPATION = "user_occupation"
+        private const val KEY_SPECIALIZATION = "user_specialization"
     }
 }
 

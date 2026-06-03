be21ba7fabe21bf03449aa0a970258a5642e437b
Vinneth <anandakevinputra208@gmail.com>
2026-06-03 11:23:59 +0700

    chores: fixing oauth one tap probelm LESGOOOO DAH JADIII

--- app/src/main/java/com/example/trackerinmobile/core/Constants.kt
+++ app/src/main/java/com/example/trackerinmobile/core/Constants.kt
@@ -1,6 +1,5 @@
 package com.example.trackerinmobile.core
 
 object Constants {
-    const val GOOGLE_CLIENT_ID = "414743491878-191g432miocj411ais33v1rs4tpigdtk.apps.googleusercontent.com"
+    // You can add other constants here if needed
 }
-
